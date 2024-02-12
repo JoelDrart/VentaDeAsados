3d0b6e4f7b9a2659b8ff16a5300737b7501f1c20
PabloGitHub25 <ldupablo21@gmail.com>
2024-02-12 18:37:57 -0500

    .

--- Model/MActualizarCliente.php
+++ Model/MActualizarCliente.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Editar Usuario</title>
-                <link rel="stylesheet" href="../css/styleEdtar.css">
+                <link rel="stylesheet" href="../css/styleEditar.css">
             </head>
             <body>
                 <form action="../model/MActualizar.php" method="POST">
