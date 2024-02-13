0c71cc210895e7119bae91ccccdea47031f58fab
Franklin Noboa <franklinnoboa22@hotmail.com>
2024-02-13 15:23:27 -0500

    Se hizo el boton eliminar

--- Model/MMostrarDPlatos.php
+++ Model/MMostrarDPlatos.php
@@ -26,7 +26,7 @@ while ($mostrar = mysqli_fetch_array($resultado)) { //recorrer el arreglo $resul
        <td><?php echo $mostrar['descripcionPlato']?></td>
        <td><?php echo $mostrar['precio']?></td>
        <td><?php echo $mostrar['tipoId']?></td>
-       <td><button type="button" class="btn btn-outline-danger"><a style="color: red; text-decoration: none; a:hover: color:black;">ELIMINAR</a></button>
+       <td><button type="button" class="btn btn-outline-danger"><a style="color: red; text-decoration: none;" href="../Model/MEliminarPlato.php?id=<?php echo $mostrar["platoId"]?>">>ELIMINAR</a></button>
        <button type="button" class="btn btn-outline-warning">EDITAR</button></td>
        
    </tr>
