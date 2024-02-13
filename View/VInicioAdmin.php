81aa3d2c32cf5c29c3fd86cfa7cc8096d30349ca
AndresPaul07 <151625679+AndresPaul07@users.noreply.github.com>
2024-02-13 14:37:21 -0500

    Update VInicioAdmin.php

--- View/VInicioAdmin.php
+++ View/VInicioAdmin.php
@@ -22,9 +22,10 @@
             <div class ="menu-1">
                     <ul>
                         <li><a href="../controller/controlador.php?var1=6">Usuarios</a></li>
-                        <li><a href="../controller/controlador.php?var1=6">Clientes</a></li>
-                        <li><a href="#">Productos</a></li>
-                        <li><a href="#">Reportes</a></li>
+                        <li><a href=""></a> </li>
+                        <li><a href=""></a> </li>
+                        <li><a href=""></a> </li>
+                        <li><a href=""></a> </li>
                     </ul>
                 </div>
 
