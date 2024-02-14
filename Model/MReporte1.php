ed13004c11de8b395c71adbba1eba86102b6f3e0
PabloGitHub25 <ldupablo21@gmail.com>
2024-02-13 21:53:19 -0500

    Update MReporte1.php

--- Model/MReporte1.php
+++ Model/MReporte1.php
@@ -9,25 +9,40 @@ $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
-$pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(30, 6, 'UserId', 1, 0, 'C');
-$pdf->Cell(30, 6, 'UserNombre', 1, 0, 'C');
-$pdf->Cell(30, 6, 'Nombre', 1, 0, 'C');
-$pdf->Cell(30, 6, 'Email', 1, 0, 'C');
-$pdf->Cell(30, 6, 'Teléfono', 1, 0, 'C');
-$pdf->Cell(30, 6, 'Contraseña', 1, 0, 'C');
-$pdf->Cell(30, 6, 'Tipo', 1, 0, 'C');
-$pdf->Ln(); // Salto de línea después de la fila de encabezado
+$pdf->SetFont('Arial', 'I', 4);
+$pdf->Ln(20);
+
+// Calcular el ancho total de la tabla
+$anchoTotal = 15 + 20 + 20 + 20 + 25 + 20 + 20 + 15;
+
+// Calcular la posición X para centrar la tabla
+$centroX = ($pdf->GetPageWidth() - $anchoTotal) / 2;
+
+$pdf->SetX($centroX);
+
+// Colores de fondo de las celdas (rojo claro)
+$rojoClaro = 255; // Valor máximo para obtener un rojo claro
+$pdf->SetFillColor($rojoClaro, $rojoClaro - 50, $rojoClaro - 50); // Rojo claro
+$pdf->Cell(15, 4, 'UserId', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'UserNombre', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Nombre', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Apellido', 1, 0, 'C', true);
+$pdf->Cell(25, 4, 'Email', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Teléfono', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Contraseña', 1, 0, 'C', true);
+$pdf->Cell(15, 4, 'Tipo', 1, 0, 'C', true);
+$pdf->Ln();
 
 while ($mostrar = mysqli_fetch_array($resultado)) {
-    $pdf->Cell(30, 6, $mostrar['userId'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['nombreUsuario'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['nombre'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['apellido'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['email'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['telefono'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['clave'], 1, 0, 'C');
-    $pdf->Cell(30, 6, $mostrar['tipoUsuario'], 1, 0, 'C');
+    $pdf->SetX($centroX);
+    $pdf->Cell(15, 4, $mostrar['userId'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['nombreUsuario'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['nombre'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['apellido'], 1, 0, 'C');
+    $pdf->Cell(25, 4, $mostrar['email'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['telefono'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['clave'], 1, 0, 'C');
+    $pdf->Cell(15, 4, $mostrar['tipoUsuario'], 1, 0, 'C');
     $pdf->Ln(); // Salto de línea después de cada fila
 }
 
