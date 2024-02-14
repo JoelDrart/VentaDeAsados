62c3f7d4fb543f4e8df3868134512e7837137908
Erick Joel Malan <erickjpk1234@gmail.com>
2024-02-14 04:16:11 -0500

    Actualizar imágenes y estilos de visualización de platos y reservas

--- Model/MMostrarPlatos.php
+++ Model/MMostrarPlatos.php
@@ -9,8 +9,8 @@ $resultado = mysqli_query($conexion,$sql);
 while ($mostrar = mysqli_fetch_array($resultado)) { //recorrer el arreglo $resultado  
    ?>
         <div class="card">
-            <img src="../img/platos_IMG/<?php echo $mostrar['imagenP']?>" class="card-img-top" alt="...">
-        <div class="card-body">
+            <img src="../img/platos_IMG/<?php echo $mostrar['imagenP']?>" class="card-img-top" alt="Imagen de <?php echo $mostrar['nombrePlato']; ?>">
+            <div class="card-body">
             <h3 style="color:white;" class="card-title"><?php echo $mostrar['nombrePlato']?></h3>
             <p style="color:white;" class="card-text"><?php echo $mostrar['descripcionPlato']?></p>
             <p style="color:white;" class="card-text"><b>$<?php echo $mostrar['precio']?></b></p>
