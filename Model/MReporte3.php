28cd4f8d77ad869a9fc93dc71efc3cf13b41e689
ErickS <ericksanchez0708@gmail.com>
2024-02-14 11:57:03 -0500

    REPORTES

--- Model/MReporte3.php
+++ Model/MReporte3.php
@@ -60,6 +60,57 @@ while ($mostrar = mysqli_fetch_array($resultado)) {
     $pdf->Ln(); // Salto de línea después de cada fila
 }
 
-$pdf->Output('I');
+//Detalles
+
+$sql = "SELECT R.reservaId AS ReservaID, P.nombrePlato AS Plato, DR.cantidad AS Cantidad, DR.subtotal AS Subtotal
+FROM reserva R JOIN detallereserva DR ON R.reservaId = DR.reservaId
+JOIN plato P ON DR.platoId = P.platoId
+ORDER BY R.reservaId ASC
+";
+// Perform the query
+$resultado = mysqli_query($conexion, $sql);
+$pdf->Ln(20);
+    // $pdf->FancyReservaTable($header,$data);
+    // $pdf->Output();
+
+    // Calcular el ancho total de la tabla
+$anchoTotal = 15 + 20 + 20 + 20;
+
+// Calcular la posición X para centrar la tabla
+$centroX = ($pdf->GetPageWidth() - $anchoTotal) / 2;
+
+$pdf->SetX($centroX);
+
+// Colores de fondo de las celdas (rojo claro)
+$rojoClaro = 255; // Valor máximo para obtener un rojo claro
+$pdf->SetFillColor($rojoClaro, $rojoClaro - 50, $rojoClaro - 50); // Rojo claro
+$pdf->Cell(15, 4, 'IDReserva', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Plato', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Cantidad', 1, 0, 'C', true);
+$pdf->Cell(20, 4, 'Subtotal', 1, 0, 'C', true);
+// $pdf->Cell(25, 4, 'Tipo', 1, 0, 'C', true);
+// $pdf->Cell(20, 4, 'TotalCobrar', 1, 0, 'C', true);
+// $pdf->Cell(20, 4, 'Contraseña', 1, 0, 'C', true);
+// $pdf->Cell(15, 4, 'Tipo', 1, 0, 'C', true);
+$pdf->Ln();
+
+while ($mostrar = mysqli_fetch_array($resultado)) {
+    $pdf->SetX($centroX);
+    $pdf->Cell(15, 4, $mostrar['ReservaID'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['Plato'], 1, 0, 'C');
+    $pdf->Cell(20, 4, $mostrar['Cantidad'], 1, 0, 'C');
+    $pdf->Cell(20, 4, '$'.$mostrar['Subtotal'], 1, 0, 'C');
+    // $pdf->Cell(25, 4, $mostrar['Tipo'], 1, 0, 'C');
+    // $pdf->Cell(20, 4, $mostrar['TotalCobrar'], 1, 0, 'C');
+    // $pdf->Cell(20, 4, $mostrar['clave'], 1, 0, 'C');
+    // $pdf->Cell(15, 4, $mostrar['tipoUsuario'], 1, 0, 'C');
+    $pdf->Ln(); // Salto de línea después de cada fila
+}
+
+
+
+
+
+$pdf->Output('I','Reportes.pdf');
 
 ?>
